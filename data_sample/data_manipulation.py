f5e3f2fff3a140384272660b25b4c0fd2c86b786
daniel <daniel@moonpass.ai>
2022-09-17 17:10:06 +0200

    Encapsulate lens methods

--- data_sample/data_manipulation.py
+++ data_sample/data_manipulation.py
@@ -3,14 +3,8 @@ import json
 import pandas as pd
 from transformers import pipeline
 
-model_path = "cardiffnlp/twitter-xlm-roberta-base-sentiment"
-sentiment_task = pipeline("sentiment-analysis", model=model_path, tokenizer=model_path)
 
-if __name__ == "__main__":
-
-    with open('posts.json') as f:
-        lens_data = json.load(f)
-
+def convert_lens_data_to_df(lens_data: dict) -> pd.DataFrame:
     items = lens_data['data']['explorePublications']['items']
     content_list, created_list = [], []
     for item in items:
@@ -22,9 +16,27 @@ if __name__ == "__main__":
     df['created_at'] = created_list
     df.created_at = pd.to_datetime(df.created_at)
 
-    sentiment_list = list(map(sentiment_task, content_list))
-    df['sentiment'] = sentiment_list
-    df['sentiment_label'] = df.sentiment.apply(lambda x: x[0]['label'])
-    df['sentiment_score'] = df.sentiment.apply(lambda x: x[0]['score'])
+    return df
+
+
+def get_sentiment_data(df: pd.DataFrame) -> pd.DataFrame:
+    model_path = "cardiffnlp/twitter-xlm-roberta-base-sentiment"
+    sentiment_task = pipeline("sentiment-analysis", model=model_path, tokenizer=model_path)
+    df['sentiment_dict'] = df.content.apply(lambda x: sentiment_task(x))
+    df['sentiment_label'] = df.sentiment_dict.apply(lambda x: x[0]['label'])
+    df['sentiment_score'] = df.sentiment_dict.apply(lambda x: x[0]['score'])
+
+    df.drop(columns=['sentiment_dict'], inplace=True)
+
+    return df
+
+
+if __name__ == "__main__":
+
+    with open('posts.json') as f:
+        lens_data = json.load(f)
+
+    df_lens = convert_lens_data_to_df(lens_data=lens_data)
+    df_lens_sentiment = get_sentiment_data(df=df_lens)
 
     print("Run")
