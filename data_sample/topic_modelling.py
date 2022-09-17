0aab0a57780cf26232ed73474ed344fbf52e4933
daniel <daniel@moonpass.ai>
2022-09-17 23:52:04 +0200

    Dump figures to a pickle

--- data_sample/topic_modelling.py
+++ data_sample/topic_modelling.py
@@ -9,22 +9,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from data_sample.data_manipulation import convert_lens_data_to_df
+import pickle
 
 
-def plot_figures(created_at: list, posts: list, topic_model: BERTopic, topics) -> None:
+def save_figures(created_at: list, posts: list, topic_model: BERTopic, topics) -> None:
     topics_over_time = topic_model.topics_over_time(posts, topics, created_at, nr_bins=20)
     fig = topic_model.visualize_topics_over_time(topics_over_time, top_n_topics=10)
-    fig.show('browser')
+    pickle.dump(fig, open("plot_ts.pickle", "wb"))
 
     fig_2 = topic_model.visualize_topics()
-    fig_2.show('browser')
+    pickle.dump(fig_2, open("plot_2d.pickle", "wb"))
 
     fig_3 = topic_model.visualize_barchart()
-    fig_3.show('browser')
+    pickle.dump(fig_3, open("plot_charts.pickle", "wb"))
 
 
 if __name__ == "__main__":
-
     # Example https://stackoverflow.com/questions/40995251/reading-an-json-file-from-s3-using-python-boto3
     s3 = boto3.client('s3', aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
                       aws_secret_access_key=os.environ['AWS_SECRET_KEY'])
@@ -42,4 +42,4 @@ if __name__ == "__main__":
 
     topic_model = BERTopic(verbose=True, n_gram_range=(1, 3), min_topic_size=7)
     topics, probs = topic_model.fit_transform(posts)
-    plot_figures(created_at=created_at, posts=posts, topic_model=topic_model, topics=topics)
+    save_figures(created_at=created_at, posts=posts, topic_model=topic_model, topics=topics)
