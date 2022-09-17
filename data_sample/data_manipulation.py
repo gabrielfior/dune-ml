0632192ea117f7dc40233a0aabe86743ddfaede2
daniel <daniel@moonpass.ai>
2022-09-17 15:55:57 +0200

    Compute sentiment analysis of posts and comments

--- data_sample/data_manipulation.py
+++ data_sample/data_manipulation.py
@@ -1,5 +1,8 @@
 import json
+from transformers import pipeline
 
+model_path = "cardiffnlp/twitter-xlm-roberta-base-sentiment"
+sentiment_task = pipeline("sentiment-analysis", model=model_path, tokenizer=model_path)
 
 if __name__ == "__main__":
 
@@ -11,5 +14,7 @@ if __name__ == "__main__":
     content = []
     for item in items:
         content.append(item['metadata']['content'])
+
+    sentiment_list = list(map(sentiment_task, content))
 
     print("Run")
