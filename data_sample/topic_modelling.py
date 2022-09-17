273c9161a237145428caa85596fe5de4932a0a6d
daniel <daniel@moonpass.ai>
2022-09-17 23:28:19 +0200

    Read posts from s3 instead of hardcoded

--- data_sample/topic_modelling.py
+++ data_sample/topic_modelling.py
@@ -1,13 +1,16 @@
 import json
+import os
 
-import pandas as pd
+import boto3
 from bertopic import BERTopic
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from data_sample.data_manipulation import convert_lens_data_to_df
 
 
 def plot_figures(created_at: list, posts: list, topic_model: BERTopic, topics) -> None:
-
     topics_over_time = topic_model.topics_over_time(posts, topics, created_at, nr_bins=20)
     fig = topic_model.visualize_topics_over_time(topics_over_time, top_n_topics=10)
     fig.show('browser')
@@ -20,9 +23,13 @@ def plot_figures(created_at: list, posts: list, topic_model: BERTopic, topics) -
 
 
 if __name__ == "__main__":
-    # NOTE: Script expects the file of S3 bucket as posts.json
-    with open('posts.json') as f:
-        lens_data = json.load(f)
+
+    # Example https://stackoverflow.com/questions/40995251/reading-an-json-file-from-s3-using-python-boto3
+    s3 = boto3.client('s3', aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
+                      aws_secret_access_key=os.environ['AWS_SECRET_KEY'])
+    obj = s3.get_object(Bucket='dune-ml', Key='PUBLICATIONS.json')
+    result = obj['Body'].read().decode('utf-8')
+    lens_data = json.loads(result)
 
     # Convert data into df
     df_lens = convert_lens_data_to_df(lens_data=lens_data)
